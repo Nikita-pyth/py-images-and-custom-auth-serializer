cede5240804d17b8a8e3b0acfdfde66830bd36f7
Nikita-pyth <siorito321@gmail.com>
2025-05-01 12:10:53 +0300

    buddy fixes

--- cinema/models.py
+++ cinema/models.py
@@ -41,7 +41,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(null=True, upload_to="create_custom_path")
+    image = models.ImageField(null=True, upload_to="cinema.utils.create_custom_path")
 
     class Meta:
         ordering = ["title"]
